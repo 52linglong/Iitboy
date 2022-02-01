c3353ab9fb4597b31438ebe0bbcb3d4991dc681c
dice <51982959+52linglong@users.noreply.github.com>
2022-02-01 18:36:59 +0800

    update

--- page.php
+++ page.php
@@ -1,20 +1,22 @@
-<?php 
+<?php
 /**
  * 自建页面模板
  */
-if(!defined('EMLOG_ROOT')) {exit('error!');} 
+if (!defined('EMLOG_ROOT')) {
+    exit('error!');
+}
 ?>
-<div id="content">
-<div id="contentleftt">
-	<h2 class="biaoti" id="masked"><?php echo $log_title; ?></h2>
-	<div class="xiantiao2"><img src="<?php echo TEMPLATE_URL; ?>images/xiantiao2.png"></div>
-	<?php echo $log_content; ?>
-	<?php blog_comments($comments); ?>
-	<?php blog_comments_post($logid,$ckname,$ckmail,$ckurl,$verifyCode,$allow_remark); ?>	
-	<div style="clear:both;"></div>
-</div><!--end #contentleftt-->
-	
+    <div id="content">
+    <div id="contentleftt">
+        <h2 class="biaoti" id="masked"><?php echo $log_title; ?></h2>
+        <div class="xiantiao2"><img src="<?php echo TEMPLATE_URL; ?>images/xiantiao2.png"></div>
+        <?php echo $log_content; ?>
+        <?php blog_comments_post($logid, $ckname, $ckmail, $ckurl, $verifyCode, $allow_remark); ?>
+        <?php blog_comments($comments); ?>
+        <div style="clear:both;"></div>
+    </div><!--end #contentleftt-->
+
 <?php
- include View::getView('side');
- include View::getView('footer');
+include View::getView('side');
+include View::getView('footer');
 ?>
